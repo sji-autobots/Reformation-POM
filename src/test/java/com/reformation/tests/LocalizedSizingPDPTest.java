bd398697f70629568e57c5e2e3281b52f08e968e
Mohit-Kotharkar <mohit.kotharkar@sjinnovation.com>
2026-03-15 21:18:35 +0530

    Mohit's progress including blockers updated on plan sheet

--- src/test/java/com/reformation/tests/LocalizedSizingPDPTest.java
+++ src/test/java/com/reformation/tests/LocalizedSizingPDPTest.java
@@ -1,10 +1,10 @@
 package com.reformation.tests;
 
-import com.reformation.base.BaseTest;
-import com.reformation.pages.PDPPage;
+import java.time.Duration;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -12,8 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-import java.util.List;
+import com.reformation.base.BaseTest;
 
 public class LocalizedSizingPDPTest extends BaseTest {
     @Test
